dce81b5631b7773bbd603277dc904bc22c6a411b
AslamBaba <aslam91r@gmail.com>
2023-04-27 12:34:53 -0700

    Add Registration, Email Verification and its Error Handling Functionality & install dotenv

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { parsed: localEnv } = require('dotenv').config()
+const webpack = require('webpack')
 
-module.exports = nextConfig
+module.exports = {
+  // ...
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
+    return config
+  },
+}
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,45 +1,142 @@
+import { useState } from "react";
+import AWS from "aws-sdk";
+import { Amplify, Auth } from "aws-amplify";
 import MainHeader from "@/components/header/mainheader";
 import styles from "./registerstyle.module.css";
 import { Row, Col } from "react-bootstrap";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import Image from 'next/image'
-import loginVector from "../../../public/loginVector.jpg"
+import Image from "next/image";
+import loginVector from "../../../public/loginVector.jpg";
 
+Amplify.configure({
+  Auth: {
+    region: process.env.region,
+    userPoolId: process.env.userPoolId,
+    userPoolWebClientId: process.env.userPoolWebClientId,
+  },
+});
 
-interface LoginFormValues {
-  fullname: string,
-  email: string,
-  phone: string,
-  password: string,
-  confirmPassword: string,
-  gender: string,
-  userType: string
+interface RegisterFormValues {
+  fullname: string;
+  email: string;
+  phone: string;
+  password: string;
+  confirmPassword: string;
+  gender: string;
+  userType: string;
 }
 
+interface EmailVerification {
+  email: string;
+  verificationCode: string;
+}
 export default function Register() {
+  let [emailVerificationStep, setEmailVerificationStep] = useState(false);
+  let [user_type_of_currentUser, set_user_type_of_currentUser] = useState("");
+  let [resendEmail, setResendEmail] = useState("");
+  let [resendVerificationCode, setResendVerificationCode] = useState(false);
 
-  const initialValues: LoginFormValues = {
-    fullname: '',
-    email: '',
-    phone: '',
-    password: '',
-    confirmPassword: '',
-    gender: '',
-    userType: ''
-  }
+  const initialValues: RegisterFormValues = {
+    fullname: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+    userType: "",
+  };
+
+  const emailVerificationInitialValues: EmailVerification = {
+    email: "",
+    verificationCode: "",
+  };
 
   const validationSchema = Yup.object().shape({
     fullname: Yup.string().required("Required"),
-    gender: Yup.string().oneOf(['male', 'female', 'other'], 'Invalid gender').required("Required"),
-    email: Yup.string().email("Inavlid email address").required('Required'),
+    gender: Yup.string()
+      .oneOf(["male", "female", "other"], "Invalid gender")
+      .required("Required"),
+    email: Yup.string().email("Inavlid email address").required("Required"),
+    phone: Yup.string().required("Required"),
+    password: Yup.string()
+      .min(8, "Password Should be 8 characters")
+      .required("Required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password")], "Password must match")
+      .required("Required"),
+    userType: Yup.string()
+      .oneOf(["Doctors", "Patient"], "Invalid user type")
+      .required("Required"),
+  });
 
-    password: Yup.string().min(8, "Password Should be 8 characters").required('Required'),
-    confirmPassword: Yup.string().oneOf([Yup.ref('password')], 'Password must match').required('Required'),
-    userType: Yup.string().oneOf(['doctor', 'patient'], 'Invalid user type').required('Required'),
-  })
+  const emailValidationSchema = Yup.object({
+    verificationCode: Yup.string().required("Verification code is required"),
+  });
 
+  const handleSubmit = async (values: RegisterFormValues) => {
+    try {
+      let respose = await Auth.signUp({
+        username: values.email,
+        password: values.password,
+        attributes: {
+          name: values.fullname,
+          email: values.email,
+          phone_number: values.phone,
+          gender: values.gender,
+          "custom:user_type": values.userType,
+        },
+      });
+      set_user_type_of_currentUser(values.userType);
+      console.log(respose);
+      if (respose) {
+        setEmailVerificationStep(true);
+      }
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
 
+  const EmailVerifyhandleSubmit = async (values: EmailVerification) => {
+    try {
+      await Auth.confirmSignUp(values.email, values.verificationCode).then(
+        (response) => {
+          console.log("Success Email Verified");
+          console.log(response);
+
+          const cognito = new AWS.CognitoIdentityServiceProvider();
+          const params = {
+            GroupName: 'Doctors',
+            Username: "developerswagsofficial@gmail.com",
+            UserPoolId: 'us-east-1_qXUjefA2V',
+          };
+        
+          cognito.adminAddUserToGroup(params, (err, data) => {
+            if (err) {
+              console.log(err)
+            }
+            if(data){
+              console.log(data);
+            }
+          });
+        }
+      );
+      setEmailVerificationStep(false);
+    } catch (error) {
+      console.log("eroor", error);
+      setResendEmail(values.email);
+      setResendVerificationCode(true);
+    }
+  };
+
+  async function ResendCode() {
+    setResendVerificationCode(false);
+    try {
+      await Auth.resendSignUp(resendEmail);
+    } catch (err) {
+      setResendVerificationCode(true);
+    }
+  }
   return (
     <>
       <MainHeader />
@@ -48,78 +145,143 @@ export default function Register() {
         <Col lg={6}>
           <div className={styles.RegisterMainSection}>
             <h2>Register</h2>
-            <Formik
-              initialValues={initialValues}
-              validationSchema={validationSchema}
-              onSubmit={(values, { setSubmitting }) => {
-                console.log(values); // log the form values
-                setSubmitting(false);
-              }}
-            >
-              {({ isSubmitting }) => (
-                <Form className={styles.FieldContainer}>
-                  <div className={styles.Field}>
-                    <label htmlFor="fullname">Full Name</label>
-                    <Field type="text" name="fullname"  />
-                    <ErrorMessage name="fullname" component="div" />
-                  </div>
-                  <div className={styles.Field}>
-                    <label htmlFor="gender">Gender</label>
-                    <Field as="select" id="gender" name="gender">
-                      <option value="">Select gender</option>
-                      <option value="male">Male</option>
-                      <option value="female">Female</option>
-                      <option value="other">Other</option>
-                    </Field>
-                    <ErrorMessage name="gender" />
-                  </div>
-                  <div className={styles.Field}>
-                    <label htmlFor="email">Email</label>
-                    <Field type="email" name="email"  />
-                    <ErrorMessage name="email" component="div" />
-                  </div>
-                  <div className={styles.Field}>
-                    <label htmlFor="phone">Phone Number</label>
-                    <Field type="text" name="phone"  />
-                    <ErrorMessage name="phone" component="div" />
-                  </div>
-                  <div className={styles.Field}>
-                    <label htmlFor="password">Password</label>
-                    <Field type="password" name="password" />
-                    <ErrorMessage name="password" component="div" />
-                  </div>
-                  <div className={styles.Field}>
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <Field type="password" name="confirmPassword" />
-                    <ErrorMessage name="confirmPassword" component="div" />
-                  </div>
-                  <div className={styles.Field}>
-                    <label htmlFor="userType">User Type</label>
-                    <div>
-                      <label>
-                        <Field type="radio" name="userType" value="doctor" />
-                        Doctor
-                      </label>
-                      <label>
-                        <Field type="radio" name="userType" value="patient" />
-                        Patient
-                      </label>
-                    </div>
-                    <ErrorMessage name="userType" component="div" />
-                  </div>
-                  <button type="submit" disabled={isSubmitting}>
-                    Register
-                  </button>
-                </Form>
-              )}
 
-            </Formik>
+            {emailVerificationStep ? (
+              /* Email Verification Section */
+
+              <>
+                <h4>Verify Email</h4>
+                <Formik
+                  initialValues={emailVerificationInitialValues}
+                  validationSchema={emailValidationSchema}
+                  onSubmit={EmailVerifyhandleSubmit}
+                >
+                  {({ errors, touched }) => (
+                    <Form className={styles.FieldContainer}>
+                      <div className={styles.Field}>
+                        <label htmlFor="email">Email</label>
+                        <Field name="email" type="email" />
+                        {errors.email && touched.email && (
+                          <div>{errors.email}</div>
+                        )}
+                      </div>
+
+                      <div className={styles.Field}>
+                        <label htmlFor="verificationCode">
+                          Verification Code
+                        </label>
+                        <Field name="verificationCode" type="text" />
+                        {errors.verificationCode &&
+                          touched.verificationCode && (
+                            <div>{errors.verificationCode}</div>
+                          )}
+                      </div>
+
+                      <button type="submit">Verify</button>
+                    </Form>
+                  )}
+                </Formik>
+                {resendVerificationCode ? (
+                  <div className={styles.ResendCodeSection}>
+                    <span>Invalid verification code.</span>
+                    <button onClick={ResendCode}>Resend Code</button>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </>
+            ) : (
+              /* Registration Section */
+              <>
+                <Formik
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={(values, { setSubmitting }) => {
+                    console.log(values); // log the form values
+                    setSubmitting(false);
+                    handleSubmit(values);
+                  }}
+                >
+                  {({ isSubmitting }) => (
+                    <Form className={styles.FieldContainer}>
+                      <div className={styles.Field}>
+                        <label htmlFor="fullname">Full Name</label>
+                        <Field type="text" name="fullname" />
+                        <ErrorMessage name="fullname" component="div" />
+                      </div>
+                      <div className={styles.Field}>
+                        <label htmlFor="gender">Gender</label>
+                        <Field as="select" id="gender" name="gender">
+                          <option value="">Select gender</option>
+                          <option value="male">Male</option>
+                          <option value="female">Female</option>
+                          <option value="other">Other</option>
+                        </Field>
+                        <ErrorMessage name="gender" />
+                      </div>
+                      <div className={styles.Field}>
+                        <label htmlFor="email">Email</label>
+                        <Field type="email" name="email" />
+                        <ErrorMessage name="email" component="div" />
+                      </div>
+                      <div className={styles.Field}>
+                        <label htmlFor="phone">Phone Number</label>
+                        <Field type="text" name="phone" />
+                        <ErrorMessage name="phone" component="div" />
+                      </div>
+                      <div className={styles.Field}>
+                        <label htmlFor="password">Password</label>
+                        <Field type="password" name="password" />
+                        <ErrorMessage name="password" component="div" />
+                      </div>
+                      <div className={styles.Field}>
+                        <label htmlFor="confirmPassword">
+                          Confirm Password
+                        </label>
+                        <Field type="password" name="confirmPassword" />
+                        <ErrorMessage name="confirmPassword" component="div" />
+                      </div>
+                      <div className={styles.Field}>
+                        <label htmlFor="userType">User Type</label>
+                        <div>
+                          <label>
+                            <Field
+                              type="radio"
+                              name="userType"
+                              value="Doctors"
+                            />
+                            Doctor
+                          </label>
+                          <label>
+                            <Field
+                              type="radio"
+                              name="userType"
+                              value="Patient"
+                            />
+                            Patient
+                          </label>
+                        </div>
+                        <ErrorMessage name="userType" component="div" />
+                      </div>
+                      <button type="submit" disabled={isSubmitting}>
+                        Register
+                      </button>
+                    </Form>
+                  )}
+                </Formik>
+              </>
+            )}
           </div>
         </Col>
         {/* RegisterVector Part */}
         <Col lg={6}>
           <div className={styles.RegisterVectorPart}>
-                <Image src={loginVector} height={500} width={500} alt="Madrack Health"/>
+            <Image
+              src={loginVector}
+              height={500}
+              width={500}
+              alt="Madrack Health"
+            />
           </div>
         </Col>
       </Row>
