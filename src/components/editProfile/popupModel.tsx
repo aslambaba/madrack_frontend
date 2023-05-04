d531475c984ceb04f4d9db572d0dd57456f5043e
AslamBaba <aslam91r@gmail.com>
2023-05-04 00:48:58 -0700

    Add popup to edit profiles.

---
+++ src/components/editProfile/popupModel.tsx
@@ -0,0 +1,50 @@
+import { ReactNode } from 'react';
+
+type PopupModalProps = {
+  children: ReactNode;
+  onClose: () => void;
+};
+
+export default function PopupModal({ children, onClose }: PopupModalProps) {
+  return (
+    <div className="popup-modal">
+      <div className="popup-content">
+        <button className="close-button" onClick={onClose}>
+          Close
+        </button>
+        {children}
+      </div>
+      <style jsx>{`
+        .popup-modal {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100vw;
+          height: 100vh;
+          background-color: rgba(0, 0, 0, 0.5);
+          z-index: 1000;
+        }
+        .popup-content {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          background-color: #fff;
+          padding: 20px;
+          overflow-y: scroll;
+
+          border-radius: 5px;
+        }
+        .close-button {
+          position: absolute;
+          top: 10px;
+          right: 10px;
+          background-color: transparent;
+          border: none;
+          font-size: 16px;
+          cursor: pointer;
+        }
+      `}</style>
+    </div>
+  );
+}
